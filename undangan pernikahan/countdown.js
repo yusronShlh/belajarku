605be84bdc0db919a7f46906ecd4a143ccb4c617
yusron sholeh <yusronbaru12gmail.com>
2025-01-31 20:47:04 +0700

    commit baru

--- undangan pernikahan/countdown.js
+++ undangan pernikahan/countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Dec 4, 2030 08:00:00").getTime();
+var countDownDate = new Date("january 29, 2030 08:00:00").getTime();
 
 // Update the countdown every 1 second
 var x = setInterval(function() {
